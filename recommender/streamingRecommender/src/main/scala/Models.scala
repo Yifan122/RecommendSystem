4c8309b109260b43fce4598b84b355cbe24307f8
Yifan122 <zengyifan122@gmail.com>
2019-11-29 10:46:57 +0800

    streaming recommender

---
+++ recommender/streamingRecommender/src/main/scala/Models.scala
@@ -0,0 +1,76 @@
+/**
+ *
+ * @param mid       movie ID
+ * @param name      movie name
+ * @param descri    movie description
+ * @param timelong  movie duration
+ * @param issue     issue date
+ * @param shoot     shoot date
+ * @param language  language
+ * @param genres    genres
+ * @param actors    actors
+ * @param directors directors
+ */
+
+case class Movie(val mid: Int, val name: String, val descri: String, val timelong: String,
+                 val issue: String, val shoot: String, val language: String,
+                 val genres: String, val actors: String, val directors: String)
+
+/**
+ *
+ * @param uid       user ID
+ * @param mid       movie ID
+ * @param score     score
+ * @param timestamp create time
+ */
+case class MovieRating(val uid: Int, val mid: Int, val score: Double, val timestamp: Int)
+
+/**
+ *
+ * @param uid       user ID
+ * @param mid       movie ID
+ * @param tag       tags created by the user
+ * @param timestamp create time
+ */
+case class Tag(val uid: Int, val mid: Int, val tag: String, val timestamp: Int)
+
+/**
+ * Mongo Configuration
+ *
+ * @param uri database uri
+ * @param db  database name
+ */
+case class MongoConfig(val uri: String, val db: String)
+
+/**
+ * Store the movie and it's score
+ *
+ * @param mid   movie id
+ * @param score movie score
+ */
+case class Recommendation(mid: Int, score: Double)
+
+/**
+ * Store the top movies in each genre
+ *
+ * @param genre           genre
+ * @param recommendations top movies
+ */
+case class GenresRecommendations(genre: String, recommendations: Seq[Recommendation])
+
+/**
+ * Store the recommendation for each uses
+ * according to the Collaborative filtering algorithm
+ *
+ * @param userId          user ID
+ * @param recommendations top movies
+ */
+case class UserRecommendations(userId: Int, recommendations: Seq[Recommendation])
+
+/**
+ * Store the movies similarity
+ *
+ * @param movieId         movie ID
+ * @param recommendations top movies
+ */
+case class MovieSimilaritys(movieId: Int, recommendations: Seq[Recommendation])
--- recommender/streamingRecommender/src/main/scala/StreamingRecommender.scala
+++ recommender/streamingRecommender/src/main/scala/StreamingRecommender.scala
@@ -1,3 +1,27 @@
-object StreamingRecommender {
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+object StreamingRecommender {
+  def main(args: Array[String]): Unit = {
+    // Set the general parameters
+    val params = scala.collection.mutable.Map[String, Any]()
+    params += "spark.cores" -> "local[3]"
+    params += "mongo.uri" -> "mongodb://bigdata112:27017/recom"
+    params += "mongo.db" -> "recom"
+    params += "spark.executor.memory" -> "4g"
+    params += "spark.driver.memory" -> "2g"
+
+    // Configure spark
+    val conf = new SparkConf().setMaster("SatisticsApp")
+      .setMaster(params("spark.cores").asInstanceOf[String])
+      .set("spark.executor.memory", params("spark.executor.memory").asInstanceOf[String])
+      .set("spark.driver.memory", params("spark.driver.memory").asInstanceOf[String])
+
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    val sc = spark.sparkContext
+    val ssc = new StreamingContext(sc, Seconds(2))
+
+    
+  }
 }
